fe6cade05bacf60d1262d38884e483fd6878b8f2
sonnyhuddart <sonny.huddart@live.co.uk>
2016-09-21 11:20:59 +0100

    Add files via upload

---
+++ Challenge3.java
@@ -0,0 +1,29 @@
+package challenge.pkg3;
+import java.util.Scanner;
+public class Challenge3 {
+  
+    public static void main(String[] args) {
+      Scanner myScanner = new Scanner(System.in);
+        
+        System.out.println("What is your first number?");
+        
+        int answer = myScanner.nextInt();
+        
+        System.out.println("What is your second number?");
+        
+        int answer2 = myScanner.nextInt();
+        
+        System.out.println(answer + " divide " + answer2 + " = " + answer / answer2 );
+        
+       
+        
+        
+        
+        
+        
+        
+        
+      
+    }
+    
+}
